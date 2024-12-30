16c0f110831172dd6a34fc6702ee63704432c55d
ABHIJEET SRIVASTAVA <abhijeetsrivastava2189@gmail.com>
2024-12-30 17:40:56 +0530

    Changed index.ejs file, index.js, set new path

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ import env from "dotenv";
 import cookieParser from "cookie-parser";
 import router from "./routes/routes.js";
 import contactRoutes from "./routes/contactRoutes.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
 import projectRoutes from './routes/projectRoutes.js';
 // import connectDB from "./server/config/db.js";
@@ -15,9 +17,13 @@ const app = express();
 const port = process.env.PORT;
 
 /// Set EJS as the default view engine
-app.set("view engine", "ejs");
-app.set("views", "./views");
+app.set("view engine", "ejs"); // Resolve __dirname equivalent
+const __filename = fileURLToPath(
+    import.meta.url);
+const __dirname = path.dirname(__filename);
 
+// Set up views directory
+app.set("views", path.join(__dirname, "./views"));
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
